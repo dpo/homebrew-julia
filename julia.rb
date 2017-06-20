860d9c29a621b4f75304152fa426245ba87323c6
Dominique <dominique.orban@gmail.com>
2017-06-20 14:08:51 -0400

    rely on homebrew/science deps, update for 0.5.2

--- julia.rb
+++ julia.rb
@@ -1,27 +1,24 @@
-require 'formula'
-
 class GitNoDepthDownloadStrategy < GitDownloadStrategy
-  # We need the .git folder for it's information, so we clone the whole thing
+  # We need the .git folder for its information, so we clone the whole thing
   def stage
     dst = Dir.getwd
     @clone.cd do
       reset
-      safe_system 'git', 'clone', '.', dst
+      safe_system "git", "clone", ".", dst
     end
   end
 end
 
 class Julia < Formula
-  homepage 'http://julialang.org'
+  homepage "http://julialang.org"
 
   stable do
-    url 'https://github.com/JuliaLang/julia.git',
-      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "v0.5.1"
-    version "0.5.1"
+    url "https://github.com/JuliaLang/julia.git",
+      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "v0.5.2"
   end
 
   head do
-    url 'https://github.com/JuliaLang/julia.git',
+    url "https://github.com/JuliaLang/julia.git",
       :using => GitNoDepthDownloadStrategy, :shallow => false
   end
 
@@ -33,7 +30,7 @@ class Julia < Formula
     sha256 "a58c5edf56c1136b326a5f06bdf77217e93f012c2d9dbabafe2311bf0e2a8e3c" => :el_capitan
   end
 
-  depends_on "staticfloat/julia/llvm37-julia"
+  depends_on "dpo/julia/llvm37-julia"
   depends_on "pcre2"
   depends_on "gmp"
   depends_on "fftw"
@@ -44,28 +41,19 @@ class Julia < Formula
   depends_on "cmake" => :build
 
   # We have our custom formulae of arpack, openblas and suite-sparse
-  depends_on "staticfloat/julia/arpack-julia"
-  depends_on "staticfloat/julia/openblas-julia"
-  depends_on "staticfloat/julia/suite-sparse-julia"
+  depends_on "arpack"
+  depends_on "openblas"
+  depends_on "suite-sparse"
 
-  # Need this as Julia's build process is quite messy with respect to env variables
+  # Need this as Julia"s build process is quite messy with respect to env variables
   env :std
 
   # Options that can be passed to the build process
   option "system-libm", "Use system's libm instead of openlibm"
 
-  # Here we build up a list of patches to be applied
-  def patches
-    patch_list = []
-
-    # This patch ensures that suitesparse libraries are installed
-    patch_list << "https://gist.githubusercontent.com/timxzl/c6f474fa387382267723/raw/2ecb0270d83f0a167358ff2a396cd6004e1b02a0/Makefile.diff"
-    return patch_list
-  end
-
   def install
-    ENV['PLATFORM'] = 'darwin'
-    ENV['PYTHONPATH'] = ""
+    ENV["PLATFORM"] = "darwin"
+    ENV["PYTHONPATH"] = ""
 
     # Build up list of build options
     build_opts = ["prefix=#{prefix}"]
@@ -73,12 +61,9 @@ class Julia < Formula
     build_opts << "TAGGED_RELEASE_BANNER=\"homebrew-julia release\""
 
     # Tell julia about our gfortran
-    # (this enables use of gfortran-4.7 from the tap homebrew-dupes/gcc.rb)
-    if ENV.has_key? 'FC'
-      build_opts << "FC=#{ENV['FC']}"
-    end
+    build_opts << "FC=#{ENV["FC"]}" if ENV.key? "FC"
 
-    # Tell julia about our llvm-config, since it's been named nonstandardly
+    # Tell julia about our llvm-config, since it"s been named nonstandardly
     build_opts << "LLVM_CONFIG=llvm-config-3.7"
     build_opts << "LLVM_VER=3.7.1"
     ENV["CPPFLAGS"] += " -DUSE_ORCJIT "
@@ -87,7 +72,7 @@ class Julia < Formula
     build_opts << "LOCALBASE=#{prefix}"
 
     # Make sure we have space to muck around with RPATHS
-    ENV['LDFLAGS'] += " -headerpad_max_install_names"
+    ENV["LDFLAGS"] += " -headerpad_max_install_names"
 
     # Make sure Julia uses clang if the environment supports it
     build_opts << "USECLANG=1" if ENV.compiler == :clang
@@ -99,41 +84,32 @@ class Julia < Formula
     build_opts << "LIBLAPACKNAME=libopenblas"
 
     # Kudos to @ijt for these lines of code
-    ['FFTW', 'GLPK', 'GMP', 'LLVM', 'PCRE', 'BLAS', 'LAPACK', 'SUITESPARSE', 'ARPACK', 'MPFR', 'LIBGIT2'].each do |dep|
+    %w[FFTW GLPK GMP LLVM PCRE BLAS LAPACK SUITESPARSE ARPACK MPFR LIBGIT2].each do |dep|
       build_opts << "USE_SYSTEM_#{dep}=1"
     end
 
     build_opts << "USE_SYSTEM_LIBM=1" if build.include? "system-libm"
 
-    # If we're building a bottle, cut back on fancy CPU instructions
+    # If we"re building a bottle, cut back on fancy CPU instructions
     build_opts << "MARCH=core2" if build.bottle?
 
-    # call makefile to grab suitesparse libraries
-    system "make", "-C", "contrib", "-f", "repackage_system_suitesparse4.make", *build_opts
-
-    # Sneak in the fftw libraries, as julia doesn't know how to load dylibs from any place other than
-    # julia's usr/lib directory and system default paths yet; the build process fixes that after the
+    # Sneak in the fftw libraries, as julia doesn"t know how to load dylibs from any place other than
+    # julia"s usr/lib directory and system default paths yet; the build process fixes that after the
     # install step, but the bootstrapping process requires the use of the fftw libraries before then
-    ['', 'f', '_threads', 'f_threads'].each do |ext|
-      ln_s "#{Formula['fftw'].lib}/libfftw3#{ext}.dylib", "usr/lib/"
-    end
+    mkdir_p "usr/lib"
+    # ["", "f", "_threads", "f_threads"].each do |ext|
+    #   ln_s "#{Formula["fftw"].lib}/libfftw3#{ext}.dylib", "usr/lib/"
+    # end
     # Do the same for openblas, pcre, mpfr, and gmp
-    ln_s "#{Formula['openblas-julia'].opt_lib}/libopenblas.dylib", "usr/lib/"
-    ln_s "#{Formula['arpack-julia'].opt_lib}/libarpack.dylib", "usr/lib/"
-    ln_s "#{Formula['pcre2'].lib}/libpcre2-8.dylib", "usr/lib/"
-    ln_s "#{Formula['mpfr'].lib}/libmpfr.dylib", "usr/lib/"
-    ln_s "#{Formula['gmp'].lib}/libgmp.dylib", "usr/lib/"
-    ln_s "#{Formula['libgit2'].lib}/libgit2.dylib", "usr/lib/"
+    ln_s "#{Formula["openblas"].opt_lib}/libopenblas.dylib", "usr/lib/"
+    ln_s "#{Formula["arpack"].opt_lib}/libarpack.dylib", "usr/lib/"
+    ln_s "#{Formula["pcre2"].lib}/libpcre2-8.dylib", "usr/lib/"
+    ln_s "#{Formula["mpfr"].lib}/libmpfr.dylib", "usr/lib/"
+    ln_s "#{Formula["gmp"].lib}/libgmp.dylib", "usr/lib/"
+    ln_s "#{Formula["libgit2"].lib}/libgit2.dylib", "usr/lib/"
 
-    build_opts << "release"
-    build_opts << "debug"
-    system "make", *build_opts
-    build_opts.pop
-    build_opts.pop
-
-    # Install!
-    build_opts << "install"
-    system "make", *build_opts
+    system "make", "release", "debug", *build_opts
+    system "make", "install", *build_opts
   end
 
   def post_install
@@ -141,17 +117,15 @@ class Julia < Formula
     rpaths = []
 
     # Add in each key-only formula to the rpaths list
-    ['arpack-julia', 'suite-sparse-julia', 'openblas-julia'].each do |formula|
-      rpaths << "#{Formula[formula].opt_lib}"
+    ["arpack", "suite-sparse", "openblas"].each do |formula|
+      rpaths << Formula[formula].opt_lib.to_s
     end
 
     # Add in generic Homebrew and system paths, as it might not be standard system paths
     rpaths << "#{HOMEBREW_PREFIX}/lib"
 
-    # Only add this in if we're < 10.8, because after that libxstub makes our lives miserable
-    if MacOS.version < :mountain_lion
-      rpaths << "/usr/X11/lib"
-    end
+    # Only add this in if we"re < 10.8, because after that libxstub makes our lives miserable
+    rpaths << "/usr/X11/lib" if MacOS.version < :mountain_lion
 
     # Add those rpaths to the binaries
     rpaths.each do |rpath|
@@ -168,9 +142,9 @@ class Julia < Formula
     end
   end
 
-  def test
+  test do
     # Run julia-provided test suite, copied over in install step
-    if not (share + 'julia/test').exist?
+    if !(share/"julia/test").exist?
       err = "Could not find test files directory\n"
       if build.head?
         err << "Did you accidentally include --HEAD in the test invocation?"
@@ -186,17 +160,17 @@ class Julia < Formula
   def caveats
     head_flag = build.head? ? " --HEAD " : " "
     <<-EOS.undent
-    Documentation and Examples have been installed into:
-    #{share}/julia
+      Documentation and Examples have been installed into:
+      #{share}/julia
 
-    Test suite has been installed into:
-    #{share}/julia/test
+      Test suite has been installed into:
+      #{share}/julia/test
 
-    To perform a quick sanity check, run the command:
-    brew test#{head_flag}-v julia
+      To perform a quick sanity check, run the command:
+      brew test#{head_flag}-v julia
 
-    To crunch through the full test suite, run the command:
-    #{bin}/julia -e "Base.runtests()"
+      To crunch through the full test suite, run the command:
+      #{bin}/julia -e "Base.runtests()"
     EOS
   end
 end
