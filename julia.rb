d46caa3b1b989fdff139f4c384ccc6923cf5ee42
Dominique <dominique.orban@gmail.com>
2017-06-20 22:00:58 -0400

    fix macho call

--- julia.rb
+++ julia.rb
@@ -128,7 +128,7 @@ class Julia < Formula
       Dir["#{bin}/julia*"].each do |file|
         chmod 0755, file
         # quiet_system "install_name_tool", "-add_rpath", rpath, file
-        MachO::Tools.add_rpath(rpath, file)
+        MachO::Tools.add_rpath(file, rpath)
         chmod 0555, file
       end
     end
